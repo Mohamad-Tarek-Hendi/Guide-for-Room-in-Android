829ad422431bd6c0f2c1c3c1919d0b2b09feef9f
Mohammad Tarek Hendi <t.hindi@outlook.com>
2023-11-10 12:07:49 +0800

    * Create app.

---
+++ app/src/main/java/com/example/guideforroominandroid/ContactDao.kt
@@ -0,0 +1,25 @@
+package com.example.guideforroominandroid
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ContactDao {
+    @Upsert
+    suspend fun upsertContact(contact: Contact)
+
+    @Delete
+    suspend fun deleteContact(contact: Contact)
+
+    @Query("SELECT * FROM Contact ORDER BY firstName ASC")
+    fun getContactOrderedByFirstName(): Flow<List<Contact>>
+
+    @Query("SELECT * FROM Contact ORDER BY lastName ASC")
+    fun getContactOrderedByLastName(): Flow<List<Contact>>
+
+    @Query("SELECT * FROM Contact ORDER BY phoneNumber ASC")
+    fun getContactOrderedByPhoneNumber(): Flow<List<Contact>>
+}
